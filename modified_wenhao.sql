834975796b4bd11f57d38f54d8c2b8bcc8b69d02
WenHao1223 <wenhaojshs@gmail.com>
2025-01-18 20:04:00 +0800

    Drop existing tables

---
+++ modified_wenhao.sql
@@ -0,0 +1,92 @@
+-- DROP EXISTING TABLES
+
+-- Module 1: Supply Chain Coordination
+-- COMPANY, SHIPPING_DESTINATION, ORDERS, ORDER_PRODUCT, PRODUCT
+DROP TABLE IF EXISTS COMPANY CASCADE;
+DROP TABLE IF EXISTS SHIPPING_DESTINATION CASCADE;
+DROP TABLE IF EXISTS ORDERS CASCADE;
+DROP TABLE IF EXISTS ORDER_PRODUCT CASCADE;
+DROP TABLE IF EXISTS PRODUCT CASCADE;
+
+-- Module 2: Inventory and Storage Management
+-- WAREHOUSE, SECTION, EMPLOYEE, CITY,. PRODUCT_INVENTORY
+DROP TABLE IF EXISTS WAREHOUSE CASCADE;
+DROP TABLE IF EXISTS SECTION CASCADE;
+DROP TABLE IF EXISTS EMPLOYEE CASCADE;
+DROP TABLE IF EXISTS CITY CASCADE;
+DROP TABLE IF EXISTS PRODUCT_INVENTORY CASCADE;
+
+-- Module 3: Sustainable Material Collection
+-- RETURNED_HISTORY, RECYCLE_TRANSACTION, RECYCLE_ORDER, RECYCLED_ITEM
+DROP TABLE IF EXISTS RETURNED_HISTORY CASCADE;
+DROP TABLE IF EXISTS RECYCLE_TRANSACTION CASCADE;
+DROP TABLE IF EXISTS RECYCLE_ORDER CASCADE;
+DROP TABLE IF EXISTS RECYCLED_ITEM CASCADE;
+
+-- Module 4: Aftersales
+-- LOGISTIC_PROVIDER, DRIVER, PAYMENT, INVOICE, FEEDBACKS
+DROP TABLE IF EXISTS LOGISTIC_PROVIDER CASCADE;
+DROP TABLE IF EXISTS DRIVER CASCADE;
+DROP TABLE IF EXISTS PAYMENT CASCADE;
+DROP TABLE IF EXISTS INVOICE CASCADE;
+
+------------------------------------------------------------
+
+-- DROP EXISTING SEQUENCES
+
+-- Module 1: Supply Chain Coordination
+
+
+-- Module 2: Inventory and Storage Management
+
+
+-- Module 3: Sustainable Material Collection
+
+
+-- Module 4: Aftersales
+
+------------------------------------------------------------
+
+-- CREATE SEQUENCES
+
+-- Module 1: Supply Chain Coordination
+
+
+-- Module 2: Inventory and Storage Management
+
+
+-- Module 3: Sustainable Material Collection
+
+
+-- Module 4: Aftersales
+
+------------------------------------------------------------
+
+-- CREATE TABLES
+
+-- Module 1: Supply Chain Coordination
+
+
+-- Module 2: Inventory and Storage Management
+
+
+-- Module 3: Sustainable Material Collection
+
+
+-- Module 4: Aftersales
+
+
+------------------------------------------------------------
+
+-- INSERT DATA
+
+-- Module 1: Supply Chain Coordination
+
+
+-- Module 2: Inventory and Storage Management
+
+
+-- Module 3: Aftersales
+
+
+-- Module 4: Sustainable Material Collection
